30ddb2b33fc1e47f4f44fc6d5a7c74530a63aa44
adconsta <adrien.constan@gmail.com>
2024-10-31 14:33:28 +0100

    ADD : Flake8 norm on scorpion

--- Cyb00/scorpion.py
+++ Cyb00/scorpion.py
@@ -4,9 +4,9 @@ from PIL import Image, ExifTags
 from pathlib import Path
 from datetime import datetime
 
+
 class Scorpion:
     def __init__(self, filepath_lst=[]):
-   
         self.filepath_set = set()
         self.img_lst = []
         self.img_attr = [
@@ -18,22 +18,21 @@ class Scorpion:
             'height',
             'palette',
             'is_animated']
-        self.file_extension_set = (".jpg", ".jpeg", ".bmp", ".gif", ".png")    
+        self.file_extension_set = (".jpg", ".jpeg", ".bmp", ".gif", ".png")
 
-        self.attr_max_len = max(len(attr) for attr in self.img_attr) # For pretty printing
+        self.attr_max_len = max(len(attr) for attr in self.img_attr)  # For pretty printing
         self.file_extension_check(filepath_lst)
-
         return
-    
+
     def file_extension_check(self, filepath_lst=[]):
-            for filepath in filepath_lst:
-                filepath = Path(filepath)
-                if filepath.suffix.lower() not in self.file_extension_set:
-                    print(f"File [{filepath.name}] not processed - File extension must be {self.file_extension_set}")
-                else:
-                    self.filepath_set.add(filepath)
-            return
-    
+        for filepath in filepath_lst:
+            filepath = Path(filepath)
+            if filepath.suffix.lower() not in self.file_extension_set:
+                print(f"File [{filepath.name}] not processed - File extension must be {self.file_extension_set}")
+            else:
+                self.filepath_set.add(filepath)
+        return
+
     def extract_metadata(self):
         for filepath in self.filepath_set:
             try:
@@ -43,7 +42,7 @@ class Scorpion:
                 print(f"**Scorpion : Error : Exctracting {filepath} metadata\n{e}**")
 
         return
-    
+
     def print_metadata(self):
         for img in self.img_lst:
             exif = img._getexif()
@@ -55,22 +54,21 @@ class Scorpion:
             for attr in self.img_attr:
                 value = getattr(img, attr, None)
                 print(f"{attr.ljust(self.attr_max_len)}\t: {value}")
-            
+
             print("\n--EXIF Attributes--")
             exif_max_length = max(len(ExifTags.TAGS.get(tag, tag)) for tag in exif.keys())
             if exif:
                 for tag, value in exif.items():
                     tag_name = ExifTags.TAGS.get(tag, tag)  # Use human-readable tag names
-                    print(f"{tag_name.ljust(exif_max_length)}\t: {value}")
+                    print(f"{tag_name.ljust(exif_max_length)}: {value}")
 
-            if getattr(exif, "DateTimeOriginal", None) == None:
+            if getattr(exif, "DateTimeOriginal", None) is None:
                 creation_time = os.path.getctime(img.filename)
                 creation_time = datetime.fromtimestamp(creation_time).strftime('%Y-%m-%d %H:%M:%S')
                 print(f"\nDateTimeOriginal : {creation_time}")
                 print("---------------Img Info End  -----------------", end="\n\n")
-
-
         return
+
 
 def main():
 
@@ -90,6 +88,7 @@ def main():
     scorpion.print_metadata()
 
     return
+
 
 if __name__ == "__main__":
     main()
