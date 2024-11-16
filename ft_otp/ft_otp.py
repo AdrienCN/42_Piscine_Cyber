0303808f36ba5d2207eb69882ce22859a2ac50ca
adconsta <adrien.constan@gmail.com>
2024-11-16 15:44:56 +0100

    ADD : Testing option

--- ft_otp/ft_otp.py
+++ ft_otp/ft_otp.py
@@ -1,4 +1,6 @@
 import re
+import os
+import random
 import argparse
 import hmac
 import time
@@ -10,22 +12,25 @@ import subprocess
 
 # QUESTION
 
-# miss leading 0
-# Compare to oathtool
-# Why CBC  ?
 # Implement QR code ?
 # Implement UX ?
 # Doc auto / integrer 
 
-# Bad practice but required by the project
+# ---------------------------
+# Global variable
+# ---------------------------
+
+# ENcryption / DEcrytpion keyS
 aes_key = b'[\xfe\x02\x05\x8e\x8fk\xbe\x1e\xa2\xd1\xc5o\xa0\xef\xea\xcb\xca\xa7y`\x87\x1c\xc06\x17\x99U\x0b5\xcd\x9a'
 iv = b')9\xa2\x1e\xc2NBc\x11\xa2oC\x17t\xdb?'
+
+# Hash method
 sha_mode="sha512"
+
 
 # ---------------------------
 # Key generation
 # ---------------------------
-
 def check_key_format(hex_str: str):
         
         if (re.fullmatch(r'^[0-9a-fA-F]{64}$', hex_str)) is None:
@@ -50,7 +55,7 @@ def gen_totp_key(hex_file):
         hex_str = hex_file.read()
 
         if check_key_format(hex_str) == False:
-            return print_error("key must be 64 hexadecimal characters.")
+            return print("Error : key must be 64 hexadecimal characters.")
         
         byte_str = hex_str.encode('utf-8')
         encrypted_data = aes_256_encrypt(byte_str)
@@ -66,7 +71,7 @@ def gen_totp_key(hex_file):
 
 
 # ---------------------------
-# TOTP generation
+# TOTP code generation
 # ---------------------------
 
 # Create a pseudo-random Offset value
@@ -88,8 +93,7 @@ def get_last_31_bits(p: bytes):
     return s_bits
 
 
-# Truncate the (20 or 64) bytes long hash to a 31 BITS long string
-# Truncation is describred in RFC 4226
+# Truncate the (20 or 64) bytes long hash to a 31 BITS long string 
 def dynamic_truncation(hash_b: bytes):
     
     offset = get_offset(hash_b)
@@ -97,10 +101,10 @@ def dynamic_truncation(hash_b: bytes):
     s_bits = get_last_31_bits(p)  # s_bits = selected bits
     return s_bits
 
-
+# RFC 4226 : https://www.rfc-editor.org/rfc/rfc4226
 # HOTP(K, C) algorithm
-# K = Key // Secret Key shared symmetrically btw client & server
-# C = simple counter (0,1 ,2 ...) incremented after each authentication attempt 
+# K = Key       // Secret Key shared symmetrically btw client & server
+# C = Counter   // Simple count (0,1 ,2 ...) incremented after each authentication attempt 
 def hotp_algo(key : bytes, time_value: int):
     
     c = time_value.to_bytes(8, byteorder='big')
@@ -114,9 +118,10 @@ def hotp_algo(key : bytes, time_value: int):
     return f"{totp_code:06}"
 
 
-# TOTP algo, essentially a reimplementation of HOTP(K, C)
-# K = Secret key and C = simple counter (0,1 ,2 ...) incremented after each authentication attempt 
-# In TOTP C is replaced by T = current time / 30 sec
+# RFC 6238 : https://www.rfc-editor.org/rfc/rfc6238
+# TOTP algo, reimplementation of HOTP
+# TOTP reimplementat HOTP(K,C) --->  HOTP(K, T) 
+# T = current time / 30 sec
 def totp_algo(k: bytes):
 
     unix_cur_time = time.time()
@@ -132,49 +137,54 @@ def aes_256_decrypt(encrypted_key: bytes):
     key = unpad(decrypted_key, AES.block_size) 
     return key
 
-def gen_totp_code(key_file):
-
+def gen_totp_code(key_file, tester: int):
+    
     encrypted_key = key_file.read()
     key = aes_256_decrypt(encrypted_key)
-    code = totp_algo(key)
-    code_str = str(code)
-    if len(code_str) == 5:
-        code_str = "0" + code_str
-    pycode = pyotp.TOTP(base64.b32encode(key), digest=sha_mode)
+    
+    # Loop for option -t
+    for j in range(tester):
+    
+        code = totp_algo(key)
+        pycode = pyotp.TOTP(base64.b32encode(key), digest=sha_mode)
+        oathtool_output = subprocess.check_output(f"oathtool --totp={sha_mode} {key.hex()}", shell=True).decode().strip()
+        print(f'{"Ft_OTP    :":<12} {code} <--')
+        print(f'{"Oathtool  :":<12} {oathtool_output}')
+        print(f'{"PyOTP     :":<12} {pycode.now()}\n')
+        key = os.urandom(32)
 
-    # Result testing 
-    oathtool_output = subprocess.check_output(f"oathtool --totp={sha_mode} {key.hex()}", shell=True).decode().strip()
-    print(f'{"Ft_OTP    :":<12} {code}')
-    print(f'{"Oathtool  :":<12} {oathtool_output}')
-    print(f'{"PyOTP     :":<12} {pycode.now()}')
     return
 
 
-def print_error(err_msg: str):
-    print(f"Error : {err_msg}")
-    return
+# ---------------------------
+# Argument parsing
+# ---------------------------
 
 def argparsing():
     parser = argparse.ArgumentParser(
                         prog="ft_otp.py",
-                        usage="npython3 ft_otp.py [ -g [HexString64] ] [ -k [file.key] ]",
+                        usage="python3 ft_otp.py -g [HexString64]\npython3 ft_otp.py -k [file.key] -t [1-20]",
                         description="Generates Key File or TOTP Authtentication code"
-                        #"-g : Generate key file\n-t : Generate TOTP code2FA Authentication"
                         )
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-g", dest="hex_file", type=argparse.FileType("r"), help="Generate key file from hex string")
     group.add_argument("-k", dest="key_file",type=argparse.FileType("rb"), help="Generate TOTP code from key file")
+    parser.add_argument("-t", dest="tester", type=int, default=1, choices=range(1,20), help="Test ft_otp againt PyOTP and Oathtool (must be between 1-20)")
     args = parser.parse_args()
     return args
+
 
 def main():
     args = argparsing()
 
+    if args.tester and not args.key_file:
+        print("Error : Option -t can only be used together with -k")
+        return
+
     if args.hex_file is not None:
         gen_totp_key(args.hex_file)
-
     elif args.key_file is not None:
-        gen_totp_code(args.key_file)
+        gen_totp_code(args.key_file, args.tester)
     return
 
 
