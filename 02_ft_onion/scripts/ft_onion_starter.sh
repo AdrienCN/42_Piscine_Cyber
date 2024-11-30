33ae855ae53fbf2033b0fd3dc353224f5848aa2a
adconsta <adrien.constan@gmail.com>
2024-11-30 14:52:56 +0100

    ONION : Stable and finished

--- 02_ft_onion/scripts/ft_onion_starter.sh
+++ 02_ft_onion/scripts/ft_onion_starter.sh
@@ -1,5 +1,20 @@
-#!/bin/bash
 
+HDR="[42]"
+HDR_END="$HDR"
+BLUE="\033[34m"
+CLR_RST="\033[0m"
+
+LINE() {
+    local arg1="$1"
+    echo -e "$HDR $arg1 $HDR_END"
+}
+
+echo "root:adminadmin" | chpasswd
+echo "ssh user=root"
+echo "password=adminadmin"
+echo "Connect ssh  via :\"ssh -p 2121 root@localhost\""
+LINE "Onion URL : $BLUE$(cat /var/lib/tor/hidden_service/hostname)$CLR_RST"
+LINE "Please use the Onion URL in a TOR browser"
 service tor start
 service nginx restart
 service ssh start -D
