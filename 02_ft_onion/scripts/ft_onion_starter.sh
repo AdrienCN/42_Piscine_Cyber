fe38e0faf7ed823afa4b458bb96bab82fa185536
adconsta <adrien.constan@gmail.com>
2024-11-30 17:57:31 +0100

    struggling w userAllowSSh

--- 02_ft_onion/scripts/ft_onion_starter.sh
+++ 02_ft_onion/scripts/ft_onion_starter.sh
@@ -12,9 +12,11 @@ LINE() {
 echo "root:adminadmin" | chpasswd
 echo "ssh user=root"
 echo "password=adminadmin"
-echo "Connect ssh  via :\"ssh -p 2121 root@localhost\""
+echo "Connect ssh  via :\"ssh -i .ssh/onion_ed25519-p 2121 root@localhost\""
 LINE "Onion URL : $BLUE$(cat /var/lib/tor/hidden_service/hostname)$CLR_RST"
 LINE "Please use the Onion URL in a TOR browser"
 service tor start
-service nginx restart
-service ssh start -D
+#service ssh start -D
+service ssh start
+service nginx start
+sleep infinity
